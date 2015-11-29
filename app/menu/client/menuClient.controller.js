5550ad29d042438a94affdb859238f08882e07bf
Constantine Zarzhitskiy <Constantine Zarzhitskiy>
2015-11-29 02:07:45 +0200

    Impl menu

--- app/menu/client/menuClient.controller.js
+++ app/menu/client/menuClient.controller.js
@@ -5,10 +5,14 @@
       .module('distanceLearning.menuClient')
       .controller('MenuClientController', MenuClientController);
 
-  MenuClientController.$inject = [];
+  MenuClientController.$inject = ['$mdSidenav'];
 
-  function MenuClientController () {
+  function MenuClientController ($mdSidenav) {
+    var vm = this;
+    vm.menuIconURL = 'assests/images/ic_apps_black_18px.svg';
 
+    vm.toggle = function () {
+      $mdSidenav('menu-client').toggle();
+    }
   }
-
 })();
