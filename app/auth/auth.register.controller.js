0e9ac4543798da847db1722f9d4c388ac2acce72
Constantine Zarzhitskiy <Constantine Zarzhitskiy>
2015-12-30 01:15:13 +0200

    Chech repass

--- app/auth/auth.register.controller.js
+++ app/auth/auth.register.controller.js
@@ -5,9 +5,16 @@
       .module('distanceLearning.auth')
       .controller('RegisterController', RegisterController);
 
-  RegisterController.$inject = [];
+  RegisterController.$inject = ['$log'];
 
-  function RegisterController() {
+  function RegisterController($log) {
     var vm = this;
+    vm.register = function (user) {
+      if (!isValidUser(user)) { return $log.log('[VALIDATION] User date isn`t valid'); }
+    };
+
+    function isValidUser (user) {
+      return user.password === user.password2;
+    }
   }
 })();
