fd2a6cb34329cf3ff4e75ebce27864d1d5ef7951
Constantine Zarzhitskiy <Constantine Zarzhitskiy>
2015-12-29 02:24:24 +0200

    Impl login page

--- app/auth/auth.login.controller.js
+++ app/auth/auth.login.controller.js
@@ -5,10 +5,21 @@
       .module('distanceLearning.auth')
       .controller('LoginController', LoginController);
 
-  LoginController.$inject = [];
+  LoginController.$inject = [
+      '$log',
+      'LoginUtils'
+  ];
 
-  function LoginController() {
+  function LoginController($log,
+                           LoginUtils) {
     var vm = this;
-
+    vm.login = function (user) {
+      LoginUtils.login(user)
+          .then(function (ok) {
+            $log.log(ok);
+          }, function (err) {
+            $log.log(err);
+          });
+    };
   }
 })();
