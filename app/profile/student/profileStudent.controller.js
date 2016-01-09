581e6da0c313451f56f1145298572d3b17a68b38
Constantine Zarzhitskiy <Constantine Zarzhitskiy>
2016-01-09 02:39:25 +0200

    Impl user info for student profile

--- app/profile/student/profileStudent.controller.js
+++ app/profile/student/profileStudent.controller.js
@@ -9,6 +9,15 @@
 
   function ProfileStudentController() {
     var vm = this;
-
+    vm.user = {
+      name: 'Constantine',
+      surname: 'Zarzhytskyy',
+      avatar: 'assests/images/user_tmp.png',
+      birthday: '01/01/2016',
+      phone: '+380994203529',
+      slug: 'Constantine_Zarzhytskyy',
+      role: 'student',
+      email: 'student@localhost.com'
+    };
   }
 })();
