fd044da0d2b6e4f495c9b6265d9bf12108b16608
Constantine Zarzhitskiy <Constantine Zarzhitskiy>
2016-01-23 00:54:51 +0200

    Add "users" on admin menu

--- app/menu/admin/menuAdmin.controller.js
+++ app/menu/admin/menuAdmin.controller.js
@@ -12,6 +12,7 @@
   function MenuAdminController ($mdSidenav) {
     var vm = this;
     vm.menuIconURL = 'assests/images/ic_apps_black_18px.svg';
+    vm.usersIconURL = 'assests/images/ic_people_black_48px.svg';
 
     vm.toggle = function () {
       $mdSidenav('menu-client').toggle();
