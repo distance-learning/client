720f3d1035064a321a7314f6ec0c5b09117db2ec
ConstantineZarzhytskyy <constantinez@interlink-ua.com>
2016-03-07 16:33:43 +0200

    Create group form

--- app/group/group.controller.js
+++ app/group/group.controller.js
@@ -14,6 +14,11 @@
                            LoginUtils, GroupUtils) {
     var vm = this;
     vm.loading = true;
+    vm.group = {
+      name: 'Назва групи',
+      faculty: {},
+      direction: {}
+    };
 
     init();
     function init() {
@@ -23,19 +28,45 @@
       GroupUtils.getFacultyInfo()
           .then(function (ok) {
             vm.faculties = ok;
+            vm.directions = vm.faculties[0].directions;
+            vm.group.faculty.id = vm.faculties[0].id;
+            vm.group.faculty.name = vm.faculties[0].name;
+            vm.group.direction.id = vm.faculties[0].directions[0].id;
+            vm.group.direction.name = vm.faculties[0].directions[0].name;
 
-            vm.loading = false;
+            GroupUtils.getStudents()
+                .then(function (ok) {
+                  vm.students = ok;
+
+                  vm.loading = false;
+                }, function (err) {
+                  $log.log('[ERROR] GroupController.init().GroupUtils.getFacultyInfo().GroupUtils.getStudents()', err);
+                });
           }, function (err) {
             $log.log('[ERROR] GroupController.init().GroupUtils.getFacultyInfo()', err);
           });
     }
 
     vm.showFacultyDirections = function (faculty) {
-      for(var i in vm.faculties) {
+      for (var i in vm.faculties) {
         if (vm.faculties[i].id == faculty.id) {
           vm.directions = vm.faculties[i].directions;
+
+          vm.group.faculty.id = vm.faculties[i].id;
+          vm.group.faculty.name = vm.faculties[i].name;
+          vm.group.direction.id = vm.faculties[i].directions[0].id;
+          vm.group.direction.name = vm.faculties[i].directions[0].name;
         }
       }
+    };
+
+    vm.selectDirection = function (direction) {
+      vm.group.direction = direction;
+    };
+
+    vm.editGroupName = function (newName) {
+      if (!newName) { vm.group.name = 'Назва групи' }
+      else { vm.group.name = newName; }
     };
   }
 })();
--- app/group/group.service.js
+++ app/group/group.service.js
@@ -11,15 +11,31 @@
   ];
 
   function GroupUtils($q, $http,
-                        server_host) {
+                      server_host) {
     var service = {
-      getFacultyInfo: getFacultyInfo
+      getFacultyInfo: getFacultyInfo,
+      getStudents: getStudents
     };
 
     function getFacultyInfo() {
       var defer = $q.defer();
 
       $http.get(server_host + 'api/auth/faculties')
+          .success(function (ok) {
+            defer.resolve(ok);
+          })
+          .error(function (err) {
+            defer.reject(err);
+          });
+
+      return defer.promise;
+    }
+
+    function getStudents() {
+      var defer = $q.defer();
+
+      // TODO: change url for get students
+      $http.get(server_host + 'api/admin/faculties')
           .success(function (ok) {
             defer.resolve(ok);
           })
